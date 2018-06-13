d4886f9c9498ca878931268190f83eb7c9c912c1
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-06-12 22:50:43 -0400

    working on integrating server and client

--- passport/local.js
+++ passport/local.js
@@ -12,40 +12,41 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 const localStrategy = new LocalStrategy((username, password, done) => {
-    const knex = dbGet();
-    let user;
-    knex.select()
-        .from('users')
-        .where('users.username', username)
-        .first()
-        .then(results => {
-            user = results;
-        if (!user) {
-            // Return rejected promise
-            return Promise.reject({
-                reason: 'LoginError',
-                message: 'Incorrect username',
-                location: 'username'
-            });
-        }
-        return bcrypt.compare(password, user.password);
+  const knex = dbGet();
+  let user;
+  knex.select()
+    .from('users')
+    .where('users.username', username)
+    .first()
+    .then(results => {
+      user = results;
+      if (!user) {
+        throw new Error(`The username ${username} has not been registered.`);
+        // Return rejected promise
+        // return Promise.reject({
+        //   reason: 'LoginError',
+        //   message: 'Incorrect username',
+        //   location: 'username'
+        // });
+      }
+      return bcrypt.compare(password, user.password);
     })
-        .then(isValid => {
-        if (!isValid) {
-            throw new Error('The password is incorrect, please try again.');
-            // return Promise.reject({
-            //     reason: 'LoginError',
-            //     message: 'Incorrect password',
-            //     location: 'password'
-            // });
-        }
-        return done(null, user);
-        })
-        .catch(err => {
-          if (err.reason === 'LoginError') {
-            return done(null, false);
-          }
-          return done(err);
-        });
+    .then(isValid => {
+      if (!isValid) {
+        throw new Error('The password is incorrect, please try again.');
+        // return Promise.reject({
+        //     reason: 'LoginError',
+        //     message: 'Incorrect password',
+        //     location: 'password'
+        // });
+      }
+      return done(null, user);
+    })
+    .catch(err => {
+      if (err.reason === 'LoginError') {
+        return done(null, false);
+      }
+      return done(err);
+    });
 });
 module.exports = localStrategy;
--- routes/trips.js
+++ routes/trips.js
@@ -6,20 +6,20 @@ const {dbGet} = require('../db-knex');
 const { getUserId } = require('../utils/getUserId');
 
 router.get('/trips', (req, res, next)=> {
-    const knex = dbGet();
-    const userId = getUserId(req);
-    //select intermidiary 
-    knex.select('t.id', 't.user_id', 'u.fullname','ut.status', 't.name', 't.destination', 't.description', 't.arrival', 't.departure')
+  const knex = dbGet();
+  const userId = getUserId(req);
+  //select intermidiary 
+  knex.select('t.id', 't.user_id', 'u.fullname','ut.status', 't.name', 't.destination', 't.description', 't.arrival', 't.departure')
     .from('users_trips as ut')
     //join trips
     //join users
     .leftJoin('users as u', 'ut.user_id', 'u.id')
     .leftJoin('trips as t', 'ut.trip_id', 't.id')
     .then(results => {
-    console.log(res.json(results))
+      console.log(res.json(results));
     })
     .catch(err => {
-        next(err);
+      next(err);
     });
-})
+});
 module.exports = router;
