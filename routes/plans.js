c5505a217338519d4f82e0d1c6a6f083e9f33f91
biancavee15 <vasquezbmarie@gmail.com>
2018-06-14 12:32:45 -0400

    post plans endpoint

---
+++ routes/plans.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const {dbGet} = require('../db-knex');
+const { getUserId } = require('../utils/getUserId');
+
+/* =====POST TO PLANS====== */
+
+router.post('/trips/:id/plans', (req, res, next) => {
+    const knex = dbGet();
+    const { id } = req.params;
+    const userId = getUserId(req);
+    const { date, description } = req.body;
+    const newPlans = {
+        trip_id: id,
+        date,
+        description
+    }
+    knex
+        .insert(newPlans)
+        .into('plans')
+        .returning('id')
+        .then(([id]) => id)
+        .then(result => {
+            res.json(result);
+          })
+          .catch(err => {
+            next(err);
+          });
+
+});
+module.exports = router;
