6e69daaab526618a3d1bd1b18e36525d58d869c3
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-06-19 23:59:59 -0400

    Add link to plans

--- routes/plans.js
+++ routes/plans.js
@@ -8,25 +8,26 @@ const { getUserId } = require('../utils/getUserId');
 /* =====POST TO PLANS====== */
 
 router.post('/trips/:id/plans', (req, res, next) => {
-    const { id } = req.params;
-    const userId = getUserId(req);
-    const { date, description } = req.body;
-    const newPlans = {
-        trip_id: id,
-        date,
-        description
-    }
-    knex
-        .insert(newPlans)
-        .into('plans')
-        .returning('id')
-        .then(([id]) => id)
-        .then(result => {
-            res.json(result);
-          })
-          .catch(err => {
-            next(err);
-          });
+  const { id } = req.params;
+  const userId = getUserId(req);
+  const { date, description, link } = req.body;
+  const newPlans = {
+    trip_id: id,
+    date,
+    description,
+    link
+  };
+  knex
+    .insert(newPlans)
+    .into('plans')
+    .returning('id')
+    .then(([id]) => id)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
 
 });
 module.exports = router;
