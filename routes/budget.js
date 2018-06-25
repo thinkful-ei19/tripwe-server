4bca122cc0efe424b38163adbaa5b5fc2bc53661
Victoria Daug <victoria.daug@gmail.com>
2018-06-25 23:12:29 +0100

    delete budget

--- routes/budget.js
+++ routes/budget.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { knex } = require('../db-knex');
 const { getUserId } = require('../utils/getUserId');
 const util = require('util');
-const { insertTransaction, getTotalBudgetByTripId } = require('../models/budget');
+const { insertTransaction, getTotalBudgetByTripId, deleteTransactionById } = require('../models/budget');
 
 router.post('/trips/:id/transactions', async (req, res, next) => {
   const { id } = req.params;
@@ -28,6 +28,17 @@ router.post('/trips/:id/transactions', async (req, res, next) => {
   }
     res.status(201).json({ updatedBudget, transaction });
 })
+/* ===== DELETE BUDGETS BY ID ===== */
+router.delete('/transactions/:id', (req, res, next) => {
+    const transactionId = req.params.id;
 
+    const success = deleteTransactionById(transactionId);
+
+    if (success) {
+        res.status(204).json();
+    } else {
+        res.status(500).json();
+    }
+})
 
 module.exports = router;
