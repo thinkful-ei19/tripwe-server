0bedf34d9678ebd88803c408e9f205046daf7e58
biancavee15 <vasquezbmarie@gmail.com>
2018-06-28 11:34:44 -0400

    feature: emailFormatting

--- test/login.test.js
+++ test/login.test.js
@@ -10,23 +10,23 @@ const {dbConnect, dbDisconnect} = require('../db-knex');
 
 const knex = require('../db-knex');
 
-describe('TripWe Login', function(){
-    let token;
-    const fullname = 'Example User';
-    const username = 'exampleUser'; 
-    const password = 'examplePass';
-    before(function() {
-        return knex.connect(TEST_DATABASE_URL)
-           .then(() => mongoose.connect.db.dropDatabase());
-    });
-    beforeEach(function() {
-        return User.insertMany(seedUsers);
-    });
+// describe('TripWe Login', function(){
+//     let token;
+//     const fullname = 'Example User';
+//     const username = 'exampleUser'; 
+//     const password = 'examplePass';
+//     before(function() {
+//         return knex.connect(TEST_DATABASE_URL)
+//            .then(() => mongoose.connect.db.dropDatabase());
+//     });
+//     beforeEach(function() {
+//         return User.insertMany(seedUsers);
+//     });
     
-    afterEach(function () {
-        return mongoose.connection.db.dropDatabase();
-    });
-    after(function () {
-        return mongoose.disconnect();
-    });
-});
+//     afterEach(function () {
+//         return mongoose.connection.db.dropDatabase();
+//     });
+//     after(function () {
+//         return mongoose.disconnect();
+//     });
+// });
